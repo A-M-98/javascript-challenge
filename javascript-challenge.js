932ee2d7a17b52cbc77b1f36fc1b6b373cb4ced6
Afaq Munawar <afaqmunawar12@gmail.com>
2022-08-07 20:47:49 +0100

    challenge 1 complete.

---
+++ javascript-challenge.js
@@ -0,0 +1,10 @@
+// Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+
+const isNumberPositive = 1
+if (isNumberPositive > 0) {
+    console.log("True")
+} else {
+    console.log("False")
+}
+
+//Write a function that takes a number of days and converts it into an age.

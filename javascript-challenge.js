4e4f250b4188ffb152483911e43ccb23dacb4e1c
Afaq Munawar <afaqmunawar12@gmail.com>
2022-08-07 23:43:43 +0100

    Q1 and Q5 code

---
+++ javascript-challenge.js
@@ -0,0 +1,21 @@
+// Write a function that takes a number and returns true 
+//if it is a positive number and false if it is a negative number.
+
+const isNumberPositive = 1
+if (isNumberPositive > 0) {
+    console.log("True")
+} else {
+    console.log("False")
+}
+
+//Write a function that takes an array of numbers and returns true 
+//if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
+
+const allNubersPositive3 = -1;
+const allNubersPositive2 = 1;
+const allNubersPositive = 1;
+if (allNubersPositive && allNubersPositive2 && allNubersPositive3 > 0) {
+    console.log("True")
+}else {
+    console.log("False")
+}
